b385b12fce0bf08c3e1878d2b7657ea1cfff82d0
JJZHU <2996904362@qq.com>
2025-06-25 14:32:36 +0800

    VUE

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1>Welcome to FIT5032!</h1>
+    <!-- 引入 MyName 组件 -->
+    <MyName /> 
+  </div>
+</template>
+
+<script>
+// 导入组件
+import MyName from "../components/MyName.vue";
+
+export default {
+  name: "Welcome",
+  components: { MyName } // 注册组件
+};
+</script>
